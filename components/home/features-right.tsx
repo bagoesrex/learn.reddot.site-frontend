a79464a40fba14e1987f9797f34653790d8a3e6b
bagoesrex <bagoesrex@gmail.com>
2025-07-15 15:17:56 +0700

    feat(home): add improve features-left content section

--- components/home/features-right.tsx
+++ components/home/features-right.tsx
@@ -1,8 +1,43 @@
+import Image from "next/image";
+import Underline from "@/public/text-underline.svg"
+import { Card } from "../ui/card";
+import { BookOpenText, Gamepad2, Lightbulb, Users } from "lucide-react";
+
 export default function FeaturesRight() {
     return (
         <div className="w-full md:w-1/2">
-            <div className="rounded-3xl p-4 h-full">
-                <h1>Cooming Soon..</h1>
+            <div className="p-4 h-full">
+                <h2 className="text-3xl md:text-4xl font-medium leading-tight">Belajar AI Gak Harus Ribet. di <span className="text-red-600">Reddot</span>, Semua Jadi <br /> Tepat Sasaran</h2>
+                <Image src={Underline} alt="" className="pr-20" />
+                <div className="flex flex-col gap-1 mt-8 text-sm">
+                    <Card className="flex flex-row w-full items-center bg-white/60 py-2 px-3 rounded-sm">
+                        <Card className="px-3 py-2 rounded-sm bg-white/80">
+                            <BookOpenText size={14} />
+                        </Card>
+                        <p className="ml-1">Materi ringkas dan jelas</p>
+                    </Card>
+
+                    <Card className="flex flex-row w-full items-center bg-white/60 py-2 px-3 rounded-sm">
+                        <Card className="px-3 py-2 rounded-sm bg-white/80">
+                            <Lightbulb size={14} />
+                        </Card>
+                        <p className="ml-1">Contoh langsung di kehidupan nyata</p>
+                    </Card>
+
+                    <Card className="flex flex-row w-full items-center bg-white/60 py-2 px-3 rounded-sm">
+                        <Card className="px-3 py-2 rounded-sm bg-white/80">
+                            <Gamepad2 size={14} />
+                        </Card>
+                        <p className="ml-1">Interaktif & seru</p>
+                    </Card>
+
+                    <Card className="flex flex-row w-full items-center bg-white/60 py-2 px-3 rounded-sm">
+                        <Card className="px-3 py-2 rounded-sm bg-white/80">
+                            <Users size={14} />
+                        </Card>
+                        <p className="ml-1">Gratis & terbuka untuk semua kalangan</p>
+                    </Card>
+                </div>
             </div>
         </div>
     )
--- components/home/features-section.tsx
+++ components/home/features-section.tsx
@@ -5,7 +5,7 @@ export default function FeaturesSection() {
     return (
         <section
             id="intro"
-            className="flex flex-col md:flex-col min-h-[26rem] bg-gray-200 items-center pb-15 pt-10 px-5 w-full"
+            className="flex flex-col md:flex-col min-h-[26rem] bg-gray-200 items-center pb-10 pt-10 px-5 w-full"
         >
             <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-8 max-w-5xl w-full">
                 <FeaturesLeft />
