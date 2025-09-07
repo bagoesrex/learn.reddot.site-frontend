3b8baffb7ba21830ee6b22d35a3242f780de155d
bagoesrex <bagoesrex@gmail.com>
2025-09-07 17:09:01 +0700

    feat(forum): add user icon to PostCard component

--- components/forum/post-card.tsx
+++ components/forum/post-card.tsx
@@ -1,3 +1,4 @@
+import { User } from "lucide-react"
 import { Card } from "../ui/card"
 
 interface PostCardProps {
@@ -6,10 +7,15 @@ interface PostCardProps {
 
 export default function PostCard({ content }: PostCardProps) {
     return (
-        <Card className="px-4 py-2 flex flex-col gap-2 w-full justify-center rounded-md shadow-none bg-white/20">
-            <p className="text-xs">
-                {content}
-            </p>
-        </Card>
+        <div className="w-full flex flex-row gap-2 items-center">
+            <Card className="px-4 py-2 flex gap-2 w-full h-fit justify-center rounded-md shadow-none bg-white/20">
+                <p className="text-xs break-words">
+                    {content}
+                </p>
+            </Card>
+            <div className="bg-primary/70 px-1 py-1 rounded-full">
+                <User size={23} color="white" />
+            </div>
+        </div>
     )
 }
